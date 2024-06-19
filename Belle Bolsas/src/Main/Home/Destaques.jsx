b3d4851cc1e11bb3a614ad91a4fb1e92cbcc3727
Thyale MD <thyaledantas@gmail.com>
2024-06-19 13:11:30 -0300

    Colocando mais fotos das bolsas

--- Belle Bolsas/src/Main/Home/Destaques.jsx
+++ Belle Bolsas/src/Main/Home/Destaques.jsx
@@ -7,6 +7,12 @@ import BolsaDestaques3 from "../../img/BolsaDestaques3.png";
 import BolsaDestaques4 from "../../img/BolsaDestaques4.png";
 import BolsaDestaques5 from "../../img/BolsaDestaques5.png";
 import BolsaDestaques6 from "../../img/BolsaDestaques6.png";
+import BolsaN1 from "../../img/BolsaN1.png";
+import BolsaN2 from "../../img/BolsaN2.png";
+import BolsaN3 from "../../img/BolsaN3.png";
+import BolsaN4 from "../../img/BolsaN4.png";
+import BolsaN5 from "../../img/BolsaN5.png";
+import BolsaN6 from "../../img/BolsaN6.png";
 
 export default function Destaques(){
 
@@ -35,37 +41,55 @@ export default function Destaques(){
             <h1>DESTAQUES</h1>
             <div className="DivGeralDestaques">
                 <div>
-                    <img src={BolsaDestaques1}/>
+                   <div className='DivImagens'>
+                       <img src={BolsaN1} className='imagem2'/>
+                       <img className='imagem1' src={BolsaDestaques1}/>
+                    </div>
                     <p>BolsaFashion</p>
                     <p>R$ 189,99</p>
                     <input type="button" onClick={CompraFeita} value="Comprar" />
                 </div>
                 <div>
-                    <img src={BolsaDestaques2}/>
+                    <div className='DivImagens'>
+                       <img src={BolsaN2} className='imagem2'/>
+                       <img className='imagem1' src={BolsaDestaques2}/>
+                    </div>
                     <p>Bolsa Versátil</p>
                     <p>R$ 148,99</p>
                     <input type="button" onClick={CompraFeita} value="Comprar" />
                 </div>
                 <div>
-                    <img src={BolsaDestaques3}/>
+                    <div className='DivImagens'>
+                       <img src={BolsaN3} className='imagem2'/>
+                       <img className='imagem1' src={BolsaDestaques3}/>
+                    </div>
                     <p>Bolsa Transverssal</p>
                     <p>R$ 109,99</p>
                     <input type="button" onClick={CompraFeita} value="Comprar" />
                 </div>
                 <div>
-                    <img src={BolsaDestaques4}/>
+                   <div className='DivImagens'>
+                       <img src={BolsaN4} className='imagem2'/>
+                       <img className='imagem1' src={BolsaDestaques4}/>
+                    </div>
                     <p>Bolsa Deluxe</p>
                     <p>R$ 99,99</p>
                     <input type="button" onClick={CompraFeita} value="Comprar" />
                 </div>
                 <div>
-                    <img src={BolsaDestaques5}/>
+                    <div className='DivImagens'>
+                       <img src={BolsaN5} className='imagem2'/>
+                       <img className='imagem1' src={BolsaDestaques5}/>
+                    </div>
                     <p>Bolsa Verão</p>
                     <p>R$ 67,99</p>
                     <input type="button" onClick={CompraFeita} value="Comprar" />
                 </div>
                 <div>
-                    <img src={BolsaDestaques6}/>
+                    <div className='DivImagens'>
+                       <img src={BolsaN6} className='imagem2'/>
+                       <img className='imagem1' src={BolsaDestaques6}/>
+                    </div>
                     <p>Bolsa Stillus</p>
                     <p>R$ 126,99</p>
                     <input type="button" onClick={CompraFeita} value="Comprar" />
