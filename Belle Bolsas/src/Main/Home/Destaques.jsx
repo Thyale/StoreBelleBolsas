77595fba3d904ca9e3eeb1397b1b5cdbcadf4f6a
Thyale MD <thyaledantas@gmail.com>
2024-06-19 09:44:38 -0300

    Destaques

---
+++ Belle Bolsas/src/Main/Home/Destaques.jsx
@@ -0,0 +1,46 @@
+import BolsaDestaques1 from "../../img/BolsaDestaques1.png";
+import BolsaDestaques2 from "../../img/BolsaDestaques2.png";
+import BolsaDestaques3 from "../../img/BolsaDestaques3.png";
+import BolsaDestaques4 from "../../img/BolsaDestaques4.png";
+import BolsaDestaques5 from "../../img/BolsaDestaques5.png";
+import BolsaDestaques6 from "../../img/BolsaDestaques6.png";
+
+export default function Destaques(){
+    return(
+        <div id="Destaques">
+            <h1>DESTAQUES</h1>
+            <div className="DivGeralDestaques">
+                <div>
+                    <img src={BolsaDestaques1}/>
+                    <p>Nome da bolsa</p>
+                    <p>R$ 189,99</p>
+                </div>
+                <div>
+                    <img src={BolsaDestaques2}/>
+                    <p>Nome da bolsa</p>
+                    <p>R$ 148,99</p>
+                </div>
+                <div>
+                    <img src={BolsaDestaques3}/>
+                    <p>Nome da bolsa</p>
+                    <p>R$ 109,99</p>
+                </div>
+                <div>
+                    <img src={BolsaDestaques4}/>
+                    <p>Nome da bolsa</p>
+                    <p>R$ 99,99</p>
+                </div>
+                <div>
+                    <img src={BolsaDestaques5}/>
+                    <p>Nome da bolsa</p>
+                    <p>R$ 67,99</p>
+                </div>
+                <div>
+                    <img src={BolsaDestaques6}/>
+                    <p>Nome da bolsa</p>
+                    <p>R$ 126,99</p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- Belle Bolsas/src/Main/Home/Home.jsx
+++ Belle Bolsas/src/Main/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Apresentacao from "./Apresentacao";
 import Banner from "./Banner";
+import Destaques from "./Destaques";
 
 export default function Home(){
     return(
         <div id="Home">
             <Apresentacao/>
             <Banner/>
+            <Destaques/>
         </div>
     )
 }
