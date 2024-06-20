443af72a7f12fe2f91d066ee67a818ea71fdc6ac
Thyale MD <thyaledantas@gmail.com>
2024-06-20 12:13:31 -0300

    Recursos adicionais

--- Belle Bolsas/src/Main/Home/Destaques.jsx
+++ Belle Bolsas/src/Main/Home/Destaques.jsx
@@ -13,13 +13,12 @@ import BolsaN3 from "../../img/BolsaN3.png";
 import BolsaN4 from "../../img/BolsaN4.png";
 import BolsaN5 from "../../img/BolsaN5.png";
 import BolsaN6 from "../../img/BolsaN6.png";
+import Carrinho from '../Shopping/Carrinho';
 
 export default function Destaques(){
 
-    const [carrinho, setCarrinho] = useState(false);
 
-    function CompraFeita() {
-        setCarrinho(true);
+    const CompraFeitaProduto = (param) => {
 
         Swal.fire({
             icon: 'success',
@@ -28,18 +27,29 @@ export default function Destaques(){
             timer: 1500,
         });
 
-        var carrinhoIcon = window.document.querySelector("span#CarrinhoIcon");
-        if (carrinhoIcon) {
-            carrinhoIcon.style.color = "green";
-        }else{
-            carrinhoIcon.style.color = "white";
+
+        window.document.querySelector("div#Carrinho").style.height = "100%"
+
+        if (param == 1) {
+            window.document.querySelector("div.produto1").style.display = "flex"
+        }else if(param == 2){
+            window.document.querySelector("div.produto2").style.display = "flex"
+        }else if(param == 3){
+            window.document.querySelector("div.produto3").style.display = "flex"
+        }else if(param == 4){
+            window.document.querySelector("div.produto4").style.display = "flex"
+        }else if(param == 5){
+            window.document.querySelector("div.produto5").style.display = "flex"
+        }else if(param == 6){
+            window.document.querySelector("div.produto6").style.display = "flex"
         }
-    }
+      };
 
     return(
         <div id="Destaques">
             <h1>DESTAQUES</h1>
             <div className="DivGeralDestaques">
+
                 <div>
                    <div className='DivImagens'>
                        <img src={BolsaN1} className='imagem2'/>
@@ -47,8 +57,9 @@ export default function Destaques(){
                     </div>
                     <p>BolsaFashion</p>
                     <p>R$ 189,99</p>
-                    <input type="button" onClick={CompraFeita} value="Comprar" />
+                    <input type="button" onClick={() => CompraFeitaProduto(1)} value="Comprar" />
                 </div>
+
                 <div>
                     <div className='DivImagens'>
                        <img src={BolsaN2} className='imagem2'/>
@@ -56,8 +67,9 @@ export default function Destaques(){
                     </div>
                     <p>Bolsa Versátil</p>
                     <p>R$ 148,99</p>
-                    <input type="button" onClick={CompraFeita} value="Comprar" />
+                    <input type="button" onClick={() => CompraFeitaProduto(2)} value="Comprar" />
                 </div>
+
                 <div>
                     <div className='DivImagens'>
                        <img src={BolsaN3} className='imagem2'/>
@@ -65,8 +77,9 @@ export default function Destaques(){
                     </div>
                     <p>Bolsa Transverssal</p>
                     <p>R$ 109,99</p>
-                    <input type="button" onClick={CompraFeita} value="Comprar" />
+                    <input type="button" onClick={() => CompraFeitaProduto(3)} value="Comprar" />
                 </div>
+
                 <div>
                    <div className='DivImagens'>
                        <img src={BolsaN4} className='imagem2'/>
@@ -74,8 +87,9 @@ export default function Destaques(){
                     </div>
                     <p>Bolsa Deluxe</p>
                     <p>R$ 99,99</p>
-                    <input type="button" onClick={CompraFeita} value="Comprar" />
+                    <input type="button" onClick={() => CompraFeitaProduto(4)} value="Comprar" />
                 </div>
+
                 <div>
                     <div className='DivImagens'>
                        <img src={BolsaN5} className='imagem2'/>
@@ -83,8 +97,9 @@ export default function Destaques(){
                     </div>
                     <p>Bolsa Verão</p>
                     <p>R$ 67,99</p>
-                    <input type="button" onClick={CompraFeita} value="Comprar" />
+                    <input type="button" onClick={() => CompraFeitaProduto(5)} value="Comprar" />
                 </div>
+
                 <div>
                     <div className='DivImagens'>
                        <img src={BolsaN6} className='imagem2'/>
@@ -92,8 +107,9 @@ export default function Destaques(){
                     </div>
                     <p>Bolsa Stillus</p>
                     <p>R$ 126,99</p>
-                    <input type="button" onClick={CompraFeita} value="Comprar" />
+                    <input type="button" onClick={() => CompraFeitaProduto(6)} value="Comprar" />
                 </div>
+                
             </div>
         </div>
     )
--- Belle Bolsas/src/Main/Shopping/Carrinho.jsx
+++ Belle Bolsas/src/Main/Shopping/Carrinho.jsx
@@ -1,5 +1,14 @@
+import React, { useEffect } from 'react';
+import { useState } from "react";
 import Login from "../Login/Login"
 import Home from "../Home/Home"
+
+import BolsaDestaques1 from "../../img/BolsaDestaques1.png";
+import BolsaDestaques2 from "../../img/BolsaDestaques2.png";
+import BolsaDestaques3 from "../../img/BolsaDestaques3.png";
+import BolsaDestaques4 from "../../img/BolsaDestaques4.png";
+import BolsaDestaques5 from "../../img/BolsaDestaques5.png";
+import BolsaDestaques6 from "../../img/BolsaDestaques6.png";
 
 export default function Carrinho(){
     
@@ -9,12 +18,181 @@ export default function Carrinho(){
         var Carrinho = window.document.querySelector("div#Carrinho").style.display = "none"
     }
 
+    const RemoverProduto = (num) => {
+        if (num === 1) {
+            document.querySelector("div.produto1").style.display = "none";
+            setQuantidade1(1)
+        } else if (num === 2) {
+            document.querySelector("div.produto2").style.display = "none";
+            setQuantidade2(1)
+        } else if (num === 3) {
+            document.querySelector("div.produto3").style.display = "none";
+            setQuantidade3(1)
+        } else if (num === 4) {
+            document.querySelector("div.produto4").style.display = "none";
+            setQuantidade4(1)
+        } else if (num === 5) {
+            document.querySelector("div.produto5").style.display = "none";
+            setQuantidade5(1)
+        } else if (num === 6) {
+            document.querySelector("div.produto6").style.display = "none";
+            setQuantidade6(1)
+        }
+    };
+
+    const [quantidade1,setQuantidade1] = useState(1)
+    const [quantidade2,setQuantidade2] = useState(1)
+    const [quantidade3,setQuantidade3] = useState(1)
+    const [quantidade4,setQuantidade4] = useState(1)
+    const [quantidade5,setQuantidade5] = useState(1)
+    const [quantidade6,setQuantidade6] = useState(1)
+    
+
+    const AdionarMais = (quant) =>{
+        if(quant == 1){
+            setQuantidade1(quantidade1 + 1)
+        }else if(quant == 2){
+            setQuantidade2(quantidade2 + 1)
+        }else if(quant == 3){
+            setQuantidade3(quantidade3 + 1)
+        }else if(quant == 4){
+            setQuantidade4(quantidade4 + 1)
+        }else if(quant == 5){
+            setQuantidade5(quantidade5 + 1)
+        }else if(quant == 6){
+            setQuantidade6(quantidade6 + 1)
+        }
+    }
+
+    const Remover = (remov) =>{
+        if(remov == 1){
+            setQuantidade1(quantidade1 - 1)
+        }else if(remov == 2){
+            setQuantidade2(quantidade2 - 1)
+        }else if(remov == 3){
+            setQuantidade3(quantidade3 - 1)
+        }else if(remov == 4){
+            setQuantidade4(quantidade4 - 1)
+        }else if(remov == 5){
+            setQuantidade5(quantidade5 - 1)
+        }else if(remov == 6){
+            setQuantidade6(quantidade6 - 1)
+        }
+    }
+
+
     return(
         <div id="Carrinho">
-            <div className="CarrinhoVazio">
-                <p>Seu carrinho de compras ainda está vazio!</p>
-                <input onClick={ShowHome} type="button" value="Voltar a página Inicial" />
+
+            <div className="CarrinhoProdutos">
+
+                <div className="produto1" id="Produtos">
+                    <div>
+                       <img src={BolsaDestaques1}/>
+                       <div>
+                           <p className="tituloBolsa">Bolsa Fashion</p>
+                           <p>R$ 189,99</p> 
+                           <div className='QuantidadeProduto'>
+                              <p>{quantidade1}</p>
+                              <p onClick={() => Remover(1)} className='QuantidadeProdutoRemover'>-</p>
+                              <p onClick={() => AdionarMais(1)} className='QuantidadeProdutoAdicionar'>+</p>
+                           </div>
+                       </div>
+                    </div>
+                    <p onClick={() => RemoverProduto(1)} id="removerItem">Remover</p>
+                </div>
+
+                <div className="produto2" id="Produtos">
+                    <div>
+                       <img src={BolsaDestaques2}/>
+                       <div>
+                           <p className="tituloBolsa">Bolsa Versátil</p>
+                           <p>R$ 148,99</p>
+                           <div className='QuantidadeProduto'>
+                              <p>{quantidade2}</p>
+                              <p onClick={() => Remover(2)} className='QuantidadeProdutoRemover'>-</p>
+                              <p onClick={() => AdionarMais(2)} className='QuantidadeProdutoAdicionar'>+</p>
+                           </div>
+                       </div>
+                    </div>
+                    <p onClick={() => RemoverProduto(2)} id="removerItem">Remover</p>
+                </div>
+
+                <div className="produto3" id="Produtos">
+                    <div>
+                       <img src={BolsaDestaques3}/>
+                       <div>
+                           <p className="tituloBolsa">Bolsa Transverssal</p>
+                           <p>R$ 109,99</p>
+                           <div className='QuantidadeProduto'>
+                              <p>{quantidade3}</p>
+                              <p onClick={() => Remover(3)} className='QuantidadeProdutoRemover'>-</p>
+                              <p onClick={() => AdionarMais(3)} className='QuantidadeProdutoAdicionar'>+</p>
+                           </div>
+                       </div>
+                    </div>
+                    <p onClick={() => RemoverProduto(3)} id="removerItem">Remover</p>
+                </div>
+
+                <div className="produto4" id="Produtos">
+                    <div>
+                       <img src={BolsaDestaques4}/>
+                       <div>
+                           <p className="tituloBolsa">Bolsa Deluxe</p>
+                           <p>R$ 99,99</p>
+                           <div className='QuantidadeProduto'>
+                              <p>{quantidade4}</p>
+                              <p onClick={() => Remover(4)} className='QuantidadeProdutoRemover'>-</p>
+                              <p onClick={() => AdionarMais(4)} className='QuantidadeProdutoAdicionar'>+</p>
+                           </div>
+                       </div>
+                    </div>
+                    <p onClick={() => RemoverProduto(4)} id="removerItem">Remover</p>
+                </div>
+
+                <div className="produto5" id="Produtos">
+                    <div>
+                       <img src={BolsaDestaques5}/>
+                       <div>
+                           <p className="tituloBolsa">Bolsa Verão</p>
+                           <p>R$ 67,99</p>
+                           <div className='QuantidadeProduto'>
+                              <p>{quantidade5}</p>
+                              <p onClick={() => Remover(5)} className='QuantidadeProdutoRemover'>-</p>
+                              <p onClick={() => AdionarMais(5)} className='QuantidadeProdutoAdicionar'>+</p>
+                           </div>
+                       </div>
+                    </div>
+                    <p onClick={() => RemoverProduto(5)} id="removerItem">Remover</p>
+                </div>
+
+                <div className="produto6" id="Produtos">
+                    <div>
+                       <img src={BolsaDestaques6}/>
+                       <div>
+                           <p className="tituloBolsa">Bolsa Stillus</p>
+                           <p>R$ 126,99</p>
+                           <div className='QuantidadeProduto'>
+                              <p>{quantidade6}</p>
+                              <p onClick={() => Remover(6)} className='QuantidadeProdutoRemover'>-</p>
+                              <p onClick={() => AdionarMais(6)} className='QuantidadeProdutoAdicionar'>+</p>
+                           </div>
+                       </div>
+                    </div>
+                    <p onClick={() => RemoverProduto(6)} id="removerItem">Remover</p>
+                </div>
+
             </div>
+
+            <div className="CarrinhoFinalizacao">
+                <p className='CarrinhoValor'>Valor total: <strong>R$00,00</strong></p>
+                <p className='endercoentrega'>Endereço de entrega:</p>
+                <input className='btnendereco' type="text" name="enderecco" id="enderecco" placeholder='Endereço Completo' autoComplete='off'/>
+                <p className='obs'>Obs: Adicione produtos antes de acessar essa área.</p>
+                <input className='btnfinalizar' type="button" value="Finalizar compra" />
+            </div>
+
+            <input onClick={ShowHome} className='btnComprar' type="button" value="Continuar comprando!" />
         </div>
     )
 }
