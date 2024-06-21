33a84e0b72f19d9e6d6e0fb4dc5d413d8bb5f40d
Thyale MD <thyaledantas@gmail.com>
2024-06-21 13:31:30 -0300

    Update Apresentacao2.jsx

--- Belle Bolsas/src/Main/Home/Apresentacao2.jsx
+++ Belle Bolsas/src/Main/Home/Apresentacao2.jsx
@@ -2,7 +2,7 @@ export default function Apresentacao2(){
     return(
         <div className="Apresentacao2">
             <p className="Apresentacao2_Titulo">Conheça a Belle Bolsas</p>
-            <p className="Apresentacao2_texto">Na Belle Bolsas, oferecemos um atendimento personalizadoN e dedicado para garantir sua satisfação. Explore nossa coleção de bolsas elegantes e estilosas, perfeitas para cada ocasião. Estamos felizes por tê-lo conosco!</p>
+            <p className="Apresentacao2_texto">Na Belle Bolsas, oferecemos um atendimento personalizado e dedicado para garantir sua satisfação. Explore nossa coleção de bolsas elegantes e estilosas, perfeitas para cada ocasião. Estamos felizes por tê-lo conosco!</p>
         </div>
     )
 }
